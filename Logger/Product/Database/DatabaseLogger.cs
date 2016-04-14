c79215237411b5550c81cf5b47e60d39d5cc45a4
Javier Dianderas <javier.dianderas@gmail.com>
2016-04-14 16:41:19 -0500

    Se agregaron dos clases que no se habian subido por el provider de Visual Studio

---
+++ Logger/Product/Database/DatabaseLogger.cs
@@ -0,0 +1,33 @@
+﻿namespace Logger.Product.Database
+{
+    class DatabaseLogger : AbstractLogger
+    {
+        private readonly IRepository _repository;
+        public DatabaseLogger(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public override void Log(string message, int type)
+        {
+            _repository.InsertLog(message, type);
+        }
+        public override void LogMessage(string message)
+        {
+            var t = 1;
+            Log(message, t);
+        }
+
+        public override void LogWarning(string message)
+        {
+            var t = 3;
+            Log(message, t);
+        }
+
+        public override void LogError(string message)
+        {
+            var t = 2;
+            Log(message, t);
+        }
+    }
+}
---
+++ Logger/Product/File/FileLogger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Logger.Product.File
+{
+    class FileLogger : AbstractLogger
+    {
+        private readonly string _fileName;
+        private readonly IFileWrapper _file;
+        private readonly DateTime _dateTime;
+
+        public FileLogger(string fileName, IFileWrapper file, DateTime dateTime)
+        {
+            _fileName = fileName;
+            _file = file;
+            _dateTime = dateTime;
+        }
+        public override void Log(string message, int type)
+        {
+            string l = string.Empty;
+            if (!_file.Exists(_fileName))
+            {
+                l = _file.ReadAllText(_fileName);
+            }            
+            l = l + DateTime.Now.ToShortDateString() + message + "\n";
+            _file.WriteAllText(_fileName, l);            
+        }
+
+        public override void LogMessage(string message)
+        {
+            var t = 1;
+            Log(message, t);
+        }
+
+        public override void LogWarning(string message)
+        {
+            var t = 3;
+            Log(message, t);
+        }
+
+        public override void LogError(string message)
+        {
+            var t = 2;
+            Log(message, t);
+        }
+    }
+}
